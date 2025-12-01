1920f65a1ba3d007bda613593109f10c484cbb80
Banksy_said_hi <banksy.maximulus@gmail.com>
2025-12-01 21:53:41 +0100

    title fixed

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ export default function Home() {
       </div> */}
 
       {/* HAMBURGER MENU */}
-      <HamburgerMenu />
+      {/* <HamburgerMenu /> */}
 
       {/* AUDIO PLAYER */}
       <AudioPlayer />
